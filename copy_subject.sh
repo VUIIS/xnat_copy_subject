7189b849ca51d6a79e58e56b4f87a9cfb45470d5
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2020-06-03 13:24:34 -0500

    parse

--- copy_subject.sh
+++ copy_subject.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
 #
-# Copy certain DICOMs of a subject from MORGAN project to MORGAN_DWI project on XNAT
-#
-# Usage:
-#    copy_subject.sh <subject_id>
-#
-# Example:
-#    copy_subject.sh 
+# Copy certain DICOMs of a subject from one project to another on XNAT
 
-subject=${1}
 
-# Source project
-source_proj=
-
-# Destination project
-dest_proj=
-
-# Labels of scans to copy - comma-separated list
-scan_types="AnatBrain_T1W3D,DTI_HARD_2.5_iso,HARDI_2.5_iso,rDTI_APA_2.5iso"
+# Parse options
+while [[ $# -gt 0 ]]
+do
+	key="${1}"
+	case "${key}" in
+		--source_project)
+			source_project="${2}"; shift; shift ;;
+		--dest_project)
+			dest_project="${2}"; shift; shift ;;
+		--subjects)
+			subjects="${2}"; shift; shift ;;
+		--scan_types)
+			scan_types="${2}"; shift; shift ;;
+		*)
+			echo Unknown input "${1}"; shift ;;
+	esac
+done
 
 # Make temporary directory
 tmp_dir=$(mktemp -d -t copy_subject) || exit 1
 
 # Download
-Xnatdownload -p "${source_proj}" -d "${tmp_dir}" --subj "${subject}" -s ${scan_types} --rs DICOM
+Xnatdownload -p "${source_project}" -d "${tmp_dir}" --subj "${subjects}" -s ${scan_types} --rs DICOM
 
 # Replace project name in download report CSV
-cat "${tmp_dir}"/download_report.csv | sed s/^scan,${source_proj}/scan,${dest_proj}/ > "${tmp_dir}"/upload.csv
+cat "${tmp_dir}"/download_report.csv | \
+	sed s/^scan,${source_project}/scan,${dest_project}/ > \
+	"${tmp_dir}"/upload.csv
 
 # Upload
 Xnatupload --csv "${tmp_dir}"/upload.csv
 
 # Clean up
-if [ -d "${tmp_)dir}" ] ; then
+if [ -d "${tmp_dir}" ] ; then
 	rm -fr "${tmp_dir}"
 fi
